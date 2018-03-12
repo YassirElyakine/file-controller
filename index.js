1d788810da9c5d326fffa8259463b33fb8a2431f
Yassir Elyakine <yassirelyakine@gmail.com>
2018-03-12 22:20:18 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ app.set('view engine', 'ejs');
 //static files
 app.use(express.static('public'));
 
-//first route
-var display = app.get('/:file:extension', function(req, res){
+//Display the file content
+app.get('/file/:file:extension', function(req, res){
     //utilities
     var file = req.params.file,
         extension = req.params.extension;
@@ -23,8 +23,8 @@ var display = app.get('/:file:extension', function(req, res){
     }, (error) => {});
 });
 
-//second route
-var write = app.get('/:file:extension/:command/:destination:destExtension', function(req, res){
+//display the file content and then save it to another file
+app.get('/file/:file:extension/:command/:destination:destExtension', function(req, res){
     //utilities
     var file = req.params.file,
         extension = req.params.extension,
@@ -41,15 +41,56 @@ var write = app.get('/:file:extension/:command/:destination:destExtension', func
     }
 });
 
-//third route
-var DELETE = app.get('/:file:extension/:command', function(req, res){
+//delete the specified file from your directory
+app.get('/file/:file:extension/:command', function(req, res){
     //utilities
     var file = req.params.file,
         command = req.params.command,
         extension = req.params.extension;
+    //utilities in action
     if(command === 'delete') {
         fs.unlink('./' + file + extension, (error) => {});
         console.log('The file ' + file + extension + ' has been deleted');
         res.send(file + extension + ' has been removed from your computer');
     }
+});
+
+//write files and save them directly from the browser
+app.get('/data', function(req, res){
+    var user_data = req.query.data;
+    //check if the user has defined --js at index[0], means that it's a js file
+    function checkFileType_js(){
+        if(user_data.search(/--js/) === 0) {
+            fs.writeFile('./user_data.js', user_data.replace(/--js/, ''), (error) => {});
+            console.log('Saved the user data as a javascript file');
+        }
+        else {
+            console.log('This is not javascript code');
+        }
+    }
+    //check if the user has defined --txt at index[0], means that it's a txt file
+    function checkFileType_txt(){
+        if(user_data.search(/--txt/) === 0) {
+            fs.writeFile('./user_data.txt', user_data.replace(/--txt/, ''), (error) => {});
+            console.log('Saved the user data as a text file');
+        }
+        else {
+            console.log('This is not a text file');
+        }
+    }
+    //check if the user has defined --json at index[0], means that it's a JSON file
+    function checkFileType_json(){
+        if(user_data.search(/--json/) === 0) {
+            fs.writeFile('./user_data.json', user_data.replace(/--json/, ''), (error) => {});
+            console.log('Saved the user data as a json(javascript object notation file)');
+        }
+        else {
+            console.log('This is not a json file');
+        }
+    }
+    
+    res.send('hot stuff');
+    checkFileType_js();
+    checkFileType_txt();
+    checkFileType_json();
 });
